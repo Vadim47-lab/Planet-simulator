eac4c3016b3c9eb5ff67e94b2b1095fdc85bcf69
Stepanov Vadim <vadgat777@mail.ru>
2021-05-11 00:31:06 +0700

    Add files via upload

---
+++ Window_graph.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using CodeMonkey.Utils;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Window_graph : MonoBehaviour
+{
+    [SerializeField] private Sprite circleSprite;//создание спрайта, наша точка для построения графа
+    private RectTransform graphContainer;
+
+    private void Awake()
+    {
+        graphContainer = transform.Find("graphContainer").GetComponent<RectTransform>();
+        List<int> valueList = new List<int>() {5, 23, 54, 67, 98, 32, 54, 65, 32};
+        ShowGraph(valueList);
+    }
+
+    private void CreateCircle(Vector2 anchoredPosition)
+    {
+        GameObject gameObject = new GameObject("circle", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().sprite = circleSprite;
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = anchoredPosition;
+        rectTransform.sizeDelta = new Vector2(11, 11);
+        rectTransform.anchorMin = new Vector2(0,0);
+        rectTransform.anchorMax = new Vector2(0,0);
+    }
+
+    private void ShowGraph(List<int> valueList)
+    {
+        int i = 0;
+        float graphHeight = graphContainer.sizeDelta.y;
+        float yMaximum = 30f;
+        float xSize = 25f;
+        GameObject LastCircleGameObject = null;
+        for (i = 0; i < valueList.Count; i++)
+        {
+            float xPosition = i * xSize;
+            float yPosition = (valueList[i] / yMaximum) * graphHeight;
+            GameObject circleGameObject = CreateCircle(new Vector2(xPosition, yPosition));
+            if (LastCircleGameObject != null) 
+            {
+                CreateDoConnection(LastCircleGameObject.GetComponent<RectTransform>().anchoredPosition, circleGameObject.GetComponent<RectTransform>().anchoredPosition);
+            }
+            LastCircleGameObject = circleGameObject;
+        }
+    }
+
+    private void CreateDoConnection(Vector2 dotPositionA, Vector2 dotPositionB)
+    {
+        GameObject gameObject = new GameObject("dotConnection", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().color = new Color(1, 1, 1, .5f);
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        Vector2 dir = (dotPositionB - dotPositionA).normalized;
+        float distance = Vector2.Distance(dotPositionA, dotPositionB);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        rectTransform.sizeDelta = new Vector2(distance, 3f);
+        rectTransform.anchoredPosition = dotPositionA + dir * distance * .5f;
+        rectTransform.localEulerAngles = new Vector3(0, 0, UtilsClass.GetAngleFromVectorFloat(dir));
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
