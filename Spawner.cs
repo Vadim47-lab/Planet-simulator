74d7d0c82e09e316cb6f01f807521ca1fd37d658
Stepanov Vadim <vadgat777@mail.ru>
2021-05-12 17:21:09 +0700

    Add files via upload

---
+++ Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject spawn;
+
+    //Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Genarate", 0, 1f);
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Genarate()
+    {
+        Instantiate(spawn, transform.position, transform.rotation);
+    }
+}
