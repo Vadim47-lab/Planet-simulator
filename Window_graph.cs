64b473c5e6ced1f068468a01abf594bba2ff72f7
Stepanov Vadim <vadgat777@mail.ru>
2021-05-15 21:19:44 +0700

    Add files via upload

--- Window_graph.cs
+++ Window_graph.cs
@@ -12,7 +12,6 @@ public class Window_graph : MonoBehaviour
     public float CurrentTime;//считает колличество секунд
     public float GameSeconds;//количество секунд
     public List<int> valueList = new List<int>() { 1, 1 };
-    public List<int> emptyList = new List<int>() { 0, 0 };
 
     private void Awake()
     {
@@ -27,8 +26,8 @@ public class Window_graph : MonoBehaviour
         if (GameSeconds >= 10.0f)
         {
             if (Main.Sumrabbit > maxCounter2) maxCounter2 = Main.Sumrabbit;
+            CleanGraph(valueList);
             valueList.Add(Main.Sumrabbit);
-            ShowGraph(emptyList);
             ShowGraph(valueList);
             GameSeconds = 0.0f;
         }
@@ -76,6 +75,45 @@ public class Window_graph : MonoBehaviour
         GameObject gameObject = new GameObject("dotConnection", typeof(Image));
         gameObject.transform.SetParent(graphContainer, false);
         gameObject.GetComponent<Image>().color = new Color(1, 1, 1, .5f);
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        Vector2 dir = (dotPositionB - dotPositionA).normalized;
+        float distance = Vector2.Distance(dotPositionA, dotPositionB);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        rectTransform.sizeDelta = new Vector2(distance, 3f);
+        rectTransform.anchoredPosition = dotPositionA + dir * distance * .5f;
+        rectTransform.localEulerAngles = new Vector3(0, 0, UtilsClass.GetAngleFromVectorFloat(dir));
+    }
+
+    public void CleanGraph(List<int> valueList)
+    {
+        int i;
+        float graphHeight = graphContainer.sizeDelta.y; //Определяем высоту контейнера для графика
+        float graphWidth = graphContainer.sizeDelta.x; //Определяем ширину контейнера для графика
+        float yMaximum = 40;//valueList.Max; //100f; Вычисляем максимальное значение по Y для всех значений списка valueList
+        float yMin = 1;//valueList.Min; //Вычисляем минимальное значение  по Y для всех значений списка valueList
+        float xMaximum = valueList.Count - 1; //Вычисляем максимальное значение по Х для всех значений списка valueList. Оно равно количеству записей в списке.
+        float xSize = graphWidth / xMaximum; //50f;//Вычисляем нормировочный коэффициент масштабирования по X
+        float ySize = (graphHeight - 15) / (yMaximum - yMin); //100f;//Вычисляем нормировочный коэффициент масштабирования по Y
+        GameObject LastCircleGameObject = null;
+        for (i = 0; i < valueList.Count; i++)
+        {
+            float xPosition = i * xSize; //Вычисляем позицию X для очередной точки на графике
+            float yPosition = valueList[i] * ySize;//Вычисляем позицию Y для очередной точки на графике
+            GameObject circleGameObject = CreateCircle(new Vector2(xPosition, yPosition));//Строим новую точку на графике в координату xPosition, yPosition 
+            if (LastCircleGameObject != null)
+            {
+                CleanDoConnection(LastCircleGameObject.GetComponent<RectTransform>().anchoredPosition, circleGameObject.GetComponent<RectTransform>().anchoredPosition);
+            }
+            LastCircleGameObject = circleGameObject;
+        }
+    }
+
+    private void CleanDoConnection(Vector2 dotPositionA, Vector2 dotPositionB)
+    {
+        GameObject gameObject = new GameObject("dotConnection", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().color = new Color(0, 170, 255, 1f);
         RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
         Vector2 dir = (dotPositionB - dotPositionA).normalized;
         float distance = Vector2.Distance(dotPositionA, dotPositionB);
