165994bfa63c5ccd2de6de94b8f8507d28f73414
Stepanov Vadim <vadgat777@mail.ru>
2021-06-17 23:15:12 +0700

    Add files via upload

--- Window_graph.cs
+++ Window_graph.cs
@@ -8,54 +8,31 @@ using System.Linq;
 
 public class Window_graph : MonoBehaviour
 {
-    [SerializeField] private Sprite circleSprite;//создание спрайта, наша точка для построения графа
+    [SerializeField] private Sprite circleSpriteRabbit;//создание спрайта, наша точка для построения графа
+    [SerializeField] private Sprite circleSpriteFox;//создание спрайта, наша точка для построения графа
     private RectTransform graphContainer;
-    public int maxCounter2 = 0;
-    public float CurrentTime;//считает количество секунд
-    public float GameSeconds;//количество секунд
     public List<int> valueRabbitList = new List<int>() { 1, 1 };
+    public List<int> valueFoxList = new List<int>() { 1, 1 };
+    public Image imageRabbit;
+    public Image imageFox;
     int i;
-    bool refresh = false;
 
     private void Awake()
     {
         graphContainer = transform.Find("graphContainer").GetComponent<RectTransform>();
-        ShowGraph(valueRabbitList);
     }
 
     void Update()
     {
         if (valueRabbitList.Count == 20) valueRabbitList.RemoveAt(1);
-        GameSeconds = GameSeconds + Time.deltaTime;
-        CurrentTime += Time.deltaTime;
-        /*if (GameSeconds <= 0.68f)
-        {
-            Debug.Log("AI_rabbit.counter2 = " + AI_rabbit.counter2);
-            Debug.Log("Update: valueRabbitList.Count = " + valueRabbitList.Count);
-            Debug.Log("valueRabbitList[valueRabbitList.Count] = " + valueRabbitList[valueRabbitList.Count - 1]);
-        }*/
-            if (GameSeconds >= 0.25f && GameSeconds <= 0.68f)
-            {
-                refresh = true;
-                Destroy(GameObject.Find("circle"));
-                Destroy(GameObject.Find("dotConnection"));
-            }
-            if (GameSeconds >= 0.69f && refresh == true)
-            {
-                if (Main.Sumrabbit > maxCounter2) maxCounter2 = Main.Sumrabbit;
-                Debug.Log("i = " + i);
-                valueRabbitList.Add(Main.Sumrabbit);
-                ShowGraph(valueRabbitList);
-                refresh = false;
-            }
-            if (GameSeconds >= 3f) GameSeconds = 0.0f;
     }
 
     private GameObject CreateCircle(Vector2 anchoredPosition)
     {
         GameObject gameObject = new GameObject("circle", typeof(Image));
         gameObject.transform.SetParent(graphContainer, false);
-        gameObject.GetComponent<Image>().sprite = circleSprite;
+        gameObject.GetComponent<Image>().sprite = circleSpriteRabbit;
+        gameObject.GetComponent<Image>().sprite = circleSpriteFox;
         RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
         rectTransform.anchoredPosition = anchoredPosition;
         rectTransform.sizeDelta = new Vector2(5, 5);
@@ -92,50 +69,94 @@ List<int> x = new List<int>() { 5, 27, -6, 14, 70, 14, 178 };
 x.RemoveAt(2); //Будет удалён 3-ий элемент по счёту
     */
 
-    public void ShowGraph(List<int> valueList)
+    public void ShowGraph()
     {
+        GameObject[] tmp = GameObject.FindGameObjectsWithTag("Circle");
+        for (int i = 0; i < tmp.Length; i++)
+        {
+            Destroy(tmp[i]);
+        }
+
+        tmp = GameObject.FindGameObjectsWithTag("dotConnection");
+        for (int i = 0; i < tmp.Length; i++)
+        {
+            Destroy(tmp[i]);
+        }
+        valueRabbitList.Add(Main.Sumrabbit);
+        valueFoxList.Add(Main.FoxSum);
         //Перенести инициализацию на вверх
         float graphHeight = graphContainer.sizeDelta.y; //Определяем высоту контейнера для графика
         float graphWidth = graphContainer.sizeDelta.x; //Определяем ширину контейнера для графика
-        float yMaximum = 10;//valueList.Max; //100f; Вычисляем максимальное значение по Y для всех значений списка valueList
-        if (Main.Sumrabbit > 10) yMaximum = Main.Sumrabbit;
-        float yMin = 1;//valueRabbitList.Min; //Вычисляем минимальное значение по Y для всех значений списка valueList
+        float yMaximumRabbit = 10;//valueList.Max; //100f; Вычисляем максимальное значение по Y для всех значений списка valueList
+        float yMaximumFox = 10;//valueList.Max; //100f; Вычисляем максимальное значение по Y для всех значений списка valueList
+        if (Main.Sumrabbit > 10) yMaximumRabbit = Main.Sumrabbit;
+        if (Main.FoxSum > 10) yMaximumFox = valueFoxList.Max();
+        float yMinRabbit = 1;//valueRabbitList.Min; //Вычисляем минимальное значение по Y для всех значений списка valueList
+        float yMinFox = 1;//valueRabbitList.Min; //Вычисляем минимальное значение по Y для всех значений списка valueList
         //float xMaximum = valueRabbitList[valueRabbitList.Count - 1]; //Вычисляем максимальное значение по Х для всех значений списка valueList. Оно равно количеству записей в списке.
+        float xMinimum = 1;
         float xMaximum = valueRabbitList.Count - 1;
-        float xSize = graphWidth / xMaximum; //50f;//Вычисляем нормировочный коэффициент масштабирования по X
-        float ySize = (graphHeight - 15) / (yMaximum - yMin); //100f;//Вычисляем нормировочный коэффициент масштабирования по Y
-        GameObject LastCircleGameObject = null;
+        float xSize = (graphWidth - 30) / (xMaximum - xMinimum); //50f;//Вычисляем нормировочный коэффициент масштабирования по X
+        float ySizeRabbit = (graphHeight - 15) / (yMaximumRabbit - yMinRabbit); //100f;//Вычисляем нормировочный коэффициент масштабирования по Y
+        float ySizeFox = (graphHeight - 15) / (yMaximumFox - yMinFox); //100f;//Вычисляем нормировочный коэффициент масштабирования по Y
+        GameObject LastCircleGameObjectRabbit = null;
+        GameObject LastCircleGameObjectFox = null;
         for (i = 0; i < valueRabbitList.Count - 1; i++)
         {
             float xPosition = i * xSize; //Вычисляем позицию X для очередной точки на графике
-            float yPosition = valueRabbitList[i] * ySize;//Вычисляем позицию Y для очередной точки на графике
-            GameObject circleGameObject = CreateCircle(new Vector2(xPosition, yPosition));//Строим новую точку на графике в координату xPosition, yPosition 
-            if (LastCircleGameObject != null)
+            float yPositionRabbit = valueRabbitList[i] * ySizeRabbit;//Вычисляем позицию Y для очередной точки на графике
+            float yPositionFox = valueFoxList[i] * ySizeFox;//Вычисляем позицию Y для очередной точки на графике
+            GameObject circleGameObjectRabbit = CreateCircle(new Vector2(xPosition, yPositionRabbit));//Строим новую точку на графике в координату xPosition, yPosition 
+            GameObject circleGameObjectFox = CreateCircle(new Vector2(xPosition, yPositionFox));//Строим новую точку на графике в координату xPosition, yPosition 
+            circleGameObjectRabbit.tag = "Circle";
+            circleGameObjectFox.tag = "Circle";
+            if (LastCircleGameObjectRabbit != null)
             {
-                CreateDoConnection(LastCircleGameObject.GetComponent<RectTransform>().anchoredPosition, circleGameObject.GetComponent<RectTransform>().anchoredPosition);
+                CreateDoConnection(new Color(1, 1, 1, .5f), LastCircleGameObjectRabbit.GetComponent<RectTransform>().anchoredPosition, circleGameObjectRabbit.GetComponent<RectTransform>().anchoredPosition);
             }
-            LastCircleGameObject = circleGameObject;
+            LastCircleGameObjectRabbit = circleGameObjectRabbit;
+
+            if (LastCircleGameObjectFox != null)
+            {
+                CreateDoConnection(new Color(1, 0, 0, .5f), LastCircleGameObjectFox.GetComponent<RectTransform>().anchoredPosition, circleGameObjectFox.GetComponent<RectTransform>().anchoredPosition);
+            }
+            LastCircleGameObjectFox = circleGameObjectFox;
         }
     }
 
-    private void CreateDoConnection(Vector2 dotPositionA, Vector2 dotPositionB)
+    private void CreateDoConnection(Color color, Vector2 dotPositionA, Vector2 dotPositionB)
     {
         //Перенести инициализацию наверх
-        GameObject gameObject = new GameObject("dotConnection", typeof(Image));
-        gameObject.transform.SetParent(graphContainer, false);
-        gameObject.GetComponent<Image>().color = new Color(1, 1, 1, .5f);
-        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
-        Vector2 dir = (dotPositionB - dotPositionA).normalized;
-        float distance = Vector2.Distance(dotPositionA, dotPositionB);
-        rectTransform.anchorMin = new Vector2(0, 0);
-        rectTransform.anchorMax = new Vector2(0, 0);
-        rectTransform.sizeDelta = new Vector2(distance, 3f);
-        rectTransform.anchoredPosition = dotPositionA + dir * distance * .5f;
-        rectTransform.localEulerAngles = new Vector3(0, 0, UtilsClass.GetAngleFromVectorFloat(dir));
+        GameObject gameObjectRabbit = new GameObject("dotConnection", typeof(Image));
+        gameObjectRabbit.tag = "dotConnection";
+        gameObjectRabbit.transform.SetParent(graphContainer, false);
+        gameObjectRabbit.GetComponent<Image>().color = color;
+        RectTransform rectTransformRabbit = gameObjectRabbit.GetComponent<RectTransform>();
+        Vector2 dirRabbit = (dotPositionB - dotPositionA).normalized;
+        float distanceRabbit = Vector2.Distance(dotPositionA, dotPositionB);
+        rectTransformRabbit.anchorMin = new Vector2(0, 0);
+        rectTransformRabbit.anchorMax = new Vector2(0, 0);
+        rectTransformRabbit.sizeDelta = new Vector2(distanceRabbit, 3f);
+        rectTransformRabbit.anchoredPosition = dotPositionA + dirRabbit * distanceRabbit * .5f;
+        rectTransformRabbit.localEulerAngles = new Vector3(0, 0, UtilsClass.GetAngleFromVectorFloat(dirRabbit));
+
+        /*GameObject gameObjectFox = new GameObject("dotConnection", typeof(Image));
+        gameObjectFox.tag = "dotConnection";
+        gameObjectFox.transform.SetParent(graphContainer, false);
+        gameObjectFox.GetComponent<Image>().color = new Color(1, 0, 0, .5f);
+        RectTransform rectTransformFox = gameObjectFox.GetComponent<RectTransform>();
+        Vector2 dirFox = (dotPositionB - dotPositionA).normalized;
+        float distanceFox = Vector2.Distance(dotPositionA, dotPositionB);
+        rectTransformFox.anchorMin = new Vector2(0, 0);
+        rectTransformFox.anchorMax = new Vector2(0, 0);
+        rectTransformFox.sizeDelta = new Vector2(distanceFox, 3f);
+        rectTransformFox.anchoredPosition = dotPositionA + dirFox * distanceFox * .5f;
+        rectTransformFox.localEulerAngles = new Vector3(0, 0, UtilsClass.GetAngleFromVectorFloat(dirFox));*/
     }
 
     void Start()
     {
-        Debug.Log("Start: valueList.Count = " + valueRabbitList.Count);
+        InvokeRepeating("ShowGraph", 1, 1);
+        Debug.Log("Start: valueListRabbit.Count = " + valueRabbitList.Count);
     }
 }
